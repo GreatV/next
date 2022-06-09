dd34470db090a6a8c1018d425692e0564a5eba3a
Wang Xin <xinwang614@gmail.com>
2022-06-09 17:01:58 +0800

    add statusbar for display image information

--- next.cpp
+++ next.cpp
@@ -1,77 +1,134 @@
 #include "next.h"
 #include "./ui_next.h"
 
+#include <fstream>
+
 #include <QFileDialog>
-#include <QStandardPaths>
 #include <QImageReader>
 #include <QImageWriter>
 #include <QScreen>
+#include <QStandardPaths>
 
 #include <opencv2/opencv.hpp>
 
+#include "nlohmann/json.hpp"
 
-static void QImage2cvMat(QImage& image, cv::Mat& mat)
+static void QImage2cvMat(QImage &image, cv::Mat &mat)
 {
-    switch(image.format())
-    {
+    switch (image.format()) {
     case QImage::Format_ARGB32:
     case QImage::Format_RGB32:
     case QImage::Format_ARGB32_Premultiplied:
-        mat = cv::Mat(image.height(), image.width(), CV_8UC4, (void*)image.constBits(), image.bytesPerLine());
+        mat = cv::Mat(image.height(),
+                      image.width(),
+                      CV_8UC4,
+                      (void *) image.constBits(),
+                      image.bytesPerLine());
         break;
     case QImage::Format_RGB888:
-        mat = cv::Mat(image.height(), image.width(), CV_8UC3, (void*)image.constBits(), image.bytesPerLine());
+        mat = cv::Mat(image.height(),
+                      image.width(),
+                      CV_8UC3,
+                      (void *) image.constBits(),
+                      image.bytesPerLine());
         cv::cvtColor(mat, mat, cv::COLOR_BGR2RGB);
         break;
     case QImage::Format_Indexed8:
-        mat = cv::Mat(image.height(), image.width(), CV_8UC1, (void*)image.constBits(), image.bytesPerLine());
+        mat = cv::Mat(image.height(),
+                      image.width(),
+                      CV_8UC1,
+                      (void *) image.constBits(),
+                      image.bytesPerLine());
         break;
     default:
         break;
     }
 }
 
-
 next::next(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::next)
 {
+    status_message_label = nullptr;
+
     ui->setupUi(this);
 
     this->resize(QGuiApplication::primaryScreen()->availableSize() * 3 / 5);
 }
 
-
-void next::init_ui()
-{
-}
-
+void next::init_ui() {}
 
 next::~next()
 {
     delete ui;
 }
 
-
 void next::on_actionOpen_triggered()
 {
     qDebug() << "action Open is triggered.";
+    std::ifstream settings_file("./configs/settings.json");
+    nlohmann::json settings;
+    QString current_dir("~/");
+    if (settings_file)
+    {
+        settings = nlohmann::json::parse(settings_file);
+        current_dir = QString(settings.at("current_dir").get<std::string>().c_str());
+        settings_file.close();
+
+    }
     auto fileName = QFileDialog::getOpenFileName(this,
                                                  tr("Open Image File"),
-                                                 "",
+                                                 current_dir,
                                                  tr("Image Files (*.jpg;*.png;*.jpeg;*.bmp)"));
-    if (fileName.isEmpty())
-    {
+    if (fileName.isEmpty()) {
         return;
     }
+
+    auto file_info = QFileInfo(fileName);
+    settings["current_dir"] = file_info.dir().absolutePath().toStdString();
+    std::string settings_string = settings.dump();
+    std::ofstream save_settings("./config/settings.json");
+    save_settings.write(settings_string.c_str(), settings_string.size());
+    save_settings.close();
     //  cv::Mat image = cv::imread(fileName.toStdString());
     //  resize image when image is too large or too small
     //  cv::resize(image, image, cv::Size(640, 640), 0, 0, cv::INTER_LINEAR);
     //  cv::cvtColor(image, image, cv::COLOR_BGR2RGB);
     //  QImage im((uchar*)image.data, image.cols, image.rows, image.step, QImage::Format_RGB888);
     QImage im(fileName);
-    ui->labelDisplayedImage->setPixmap(QPixmap::fromImage(im));
-    ui->labelDisplayedImage->resize(ui->labelDisplayedImage->pixmap().size());
-    ui->labelDisplayedImage->setAlignment(Qt::AlignCenter);
-}
 
+    // get raw image size
+    auto raw_depth = im.depth();
+    auto raw_width = im.size().width();
+    auto raw_height = im.size().height();
+
+    // get image container's size
+    auto available_width = ui->widgetCenter->size().width();
+    auto available_height = ui->widgetCenter->size().height();
+
+    auto w = raw_width > available_width ? available_width : raw_width;
+    auto h = raw_height > available_height ? available_height : raw_height;
+
+    // calculate resize ratio of image
+    auto resize_ratio = w > h ? w * 1.0f / raw_width : h * 1.0f / raw_height;
+
+    auto scaled_size(QSize(raw_width * resize_ratio, raw_height * resize_ratio));
+    QImage dispalyed_image = im.scaled(scaled_size);
+
+    ui->labelDisplayedImage->setPixmap(QPixmap::fromImage(dispalyed_image));
+    ui->labelDisplayedImage->setAlignment(Qt::AlignCenter);
+
+    // show image information in the status bar
+    if (status_message_label) {
+        ui->statusbar->removeWidget(status_message_label);
+    } else {
+        auto message = QString("%1\% %2x%3 %4")
+                           .arg(resize_ratio * 100)
+                           .arg(raw_width)
+                           .arg(raw_height)
+                           .arg(raw_depth);
+        status_message_label = new QLabel(message);
+        status_message_label->setAlignment(Qt::AlignRight);
+        ui->statusbar->addWidget(status_message_label, 1);
+    }
+}
--- next.h
+++ next.h
@@ -2,6 +2,7 @@
 #define NEXT_H
 
 #include <QMainWindow>
+#include <QLabel>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class next; }
@@ -22,6 +23,7 @@ private:
     Ui::next *ui;
     QImage displayed_image;
 
+    QLabel* status_message_label;
     void init_ui();
 };
 #endif // NEXT_H
